7af4e8e3c466ac680bab18327b764da3e874d74b
Eze Amadi <eze.kachi.amadi@gmail.com>
2020-06-03 22:54:54 -0500

    method to extract the maximum in the max heap

--- Heaps/MaxBinaryHeap.js
+++ Heaps/MaxBinaryHeap.js
@@ -22,4 +22,51 @@ class MaxBinaryHeap {
       id = parentId;
     }
   }
+
+  extractMax() {
+    // remove the root which is the maximum in a heap
+    const max = this.value[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.trickleDown();
+    }
+
+    return max;
+  }
+
+  // trickle down
+  trickleDown() {
+    let id = 0;
+    const length = this.values.length;
+    const node = this.values[0];
+
+    while (true) {
+      let leftChildId = 2 * id + 1;
+      let rightChildId = 2 * id + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildId < length) {
+        leftChild = this.values[leftChildId];
+
+        if (leftChild > node) swap = leftChildId;
+      }
+
+      if (rightChildId < length) {
+        rightChild = this.values[rightChildId];
+        if (
+          (swap === null && rightChild > node) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildId;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[id] = this.values[swap];
+      this.values[swap] = element;
+      id = swap;
+    }
+  }
 }
